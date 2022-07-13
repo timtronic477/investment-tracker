43ce0c871eb4b3c568bba64bb0b436999e5df1ae
Timothy Balilo <ts.balilo@gmail.com>
2022-07-13 14:51:47 -0700

    added form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const Form = () => {
+    
+
+
+    return (
+        <form>
+            <div>
+            <label>Location</label>
+            <input type="text"></input>
+            </div>
+            <div>
+            <label>Date</label>
+            <input type="date"></input>
+            </div>
+            <div>
+            <label>Amount</label>
+            <input type="number"></input>
+            </div>
+        </form>
+    )
+}
+
+export default Form;
